7bc5905a1c3fa266ef4181add207fa1a0d92b466
Mat Aldao <mataldao@Mats-MacBook-Pro.local>
2024-06-13 10:08:47 -0300

    Run Prebuild

--- app.config.js
+++ app.config.js
@@ -10,18 +10,18 @@ export default ({ config }) => {
       fallbackToCacheTimeout: 0,
       "url": "https://u.expo.dev/77203b3b-264f-49bd-8e04-c3737db79889"
     },
-    "runtimeVersion": "1.0.4",
+    "runtimeVersion": "1.0.5",
     // name: process.env.NODE_ENV == 'prod' ? 'UGo!' : 'UGo! Test',
     name: 'Sparring',
     ios: {
       ...config.ios,
       bundleIdentifier:
-        // 'com.sparring.developer', // development ios bundle identifier
-        'com.testing.developer',
+        'com.sparring.developer', // development ios bundle identifier
+        // 'com.testing.developer',
 
       // googleServicesFile: './GoogleService-Info.plist',
     },
-    version: '1.10.24',
+    version: '1.10.25',
     
     "extra": {
       "eas": {
