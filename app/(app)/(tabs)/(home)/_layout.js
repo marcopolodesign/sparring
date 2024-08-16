a5606aa59f40371eb8b51b256eaa07db3b13a3bc
Mat Aldao <mataldao@Mats-MacBook-Pro.local>
2024-08-16 15:51:32 -0300

    Profile Added

--- app/(app)/(tabs)/(home)/_layout.js
+++ app/(app)/(tabs)/(home)/_layout.js
@@ -21,8 +21,8 @@ export default function Home() {
   
     }}
   >
-    <Stack.Screen name="index" options={{ headerShown: false}} />
-    {/* <Stack.Screen name="profile" /> */}
+    {/* <Stack.Screen name="index" options={{ headerShown: false}} /> */}
+    <Stack.Screen name="profile" />
     {/* <Stack.Screen name="onboarding-sport" options={{ headerShown: false }} /> */}
   </Stack>
 
